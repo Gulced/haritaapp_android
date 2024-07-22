69ff5dded75493fed1fdf5c472505198d49d0937
Gulced <114645726+Gulced@users.noreply.github.com>
2024-07-22 15:05:20 +0300

    first commit

---
+++ app/src/main/java/com/example/beltekharitaapp/MapsActivity.java
@@ -0,0 +1,43 @@
+package com.example.beltekharitaapp;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.example.beltekharitaapp.databinding.ActivityMapsBinding;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        /* Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
+        LatLng gazi= new LatLng(39.937707,32.820936);
+        mMap.addMarker(new MarkerOptions().position(gazi).title("Gazi üniversitesi teknoloji fakültesi"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(gazi,20));
+
+    }
+}
